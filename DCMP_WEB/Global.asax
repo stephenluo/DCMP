0847e224ce50ea9ac2e3f345f561d8f1494d8261
stephen luo <909709929@qq.com>
2016-05-16 16:59:12 +0800

    create DCMP Web Project

---
+++ DCMP_WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DCMP_WEB.MvcApplication" Language="C#" %>
---
+++ DCMP_WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DCMP_WEB.Startup))]
+namespace DCMP_WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
